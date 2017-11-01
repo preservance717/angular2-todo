bef392603f1f7d7312a2d1f01da31d8b820ee05c
LG <GL717@outlook.com>
2017-11-01 10:30:08 +0800

    refactor code

--- app/components/add_todo/add-todo.component.ts
+++ app/components/add_todo/add-todo.component.ts
@@ -1,17 +1,27 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
 import {Todo} from "../../app.component";
 
 @Component({
     selector: 'add-todo',
-    template: `<input type="checkbox" checked="{{todo.isDone ? 'checked':''}}" (click)="toggle(todo)">
-               <input type="text" value="{{todo.item}}">
-               <button (click)="deleteTodo(todo.id)">✘</button>`
+    // template: `<input type="checkbox" checked="{{todo.isDone ? 'checked':''}}" (click)="toggle(todo)">
+    //            <input type="text" value="{{todo.item}}">
+    //            <button (click)="deleteTodo(todo.id)">✘</button>`
+    template:`<div *ngFor="let todo of todos">
+<input type="checkbox" checked="{{todo.isDone ? 'checked':''}}" (click)="toggle(todo)">
+<span>{{todo.item}}</span>
+<button (click)="deleteTodo(todo.id)">✘</button>
+</div>`
 })
 
-export class AddTodoComponent implements Input {
-    @Input() todo;
+export class AddTodoComponent implements Input, OnChanges{
+    // @Input() todo;
+    @Input() todos;
     @Output() onDelete = new EventEmitter();
     @Output() onToggle = new EventEmitter();
+
+    ngOnChanges(){
+        console.log('change');
+    }
 
     deleteTodo(id:number){
         this.onDelete.emit(id);
